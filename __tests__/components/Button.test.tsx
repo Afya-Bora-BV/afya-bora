3d8e313857a182cd76ed3eab7a394eaa938204a3
Rkeint <keinthraghav@gmail.com>
2021-07-07 15:57:51 +0300

    avatar

--- __tests__/components/Button.test.tsx
+++ __tests__/components/Button.test.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { render } from "@testing-library/react-native";
-import { NativeBaseProvider } from 'native-base'
-import Button from "../../src/components/button";
+import { NativeBaseProvider } from "native-base";
+import Button, { PrimaryButton } from "../../src/components/button";
 
 describe("<Button/>", () => {
-    test("is it rendered appropriately", () => {
-        const { getByTestId } = render(
-            <NativeBaseProvider>
-                <Button />
-            </NativeBaseProvider>
-        );
+  test("is it rendered appropriately", () => {
+    const { getByTestId } = render(
+      <NativeBaseProvider>
+        <Button />
+      </NativeBaseProvider>
+    );
 
-        expect(getByTestId("demo")).toBeDefined();
-    });
+    expect(getByTestId("button")).toBeDefined();
+  });
 });
--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,19 +1,31 @@
 import React from "react";
-import {
-  Input,
-  Stack,
-  Center,
-  NativeBaseProvider,
-  Text,
-  Pressable,
-  HStack,
-} from "native-base";
+import { Stack, Avatar } from "native-base";
 import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
 import { colors } from "../contants/colors";
 import { Alert } from "react-native";
 import { useState } from "react";
 
-export const Avatar = () => {
-  return
-  null;
-}
+export const PicAvatar = () => {
+	return (
+		<Stack
+			direction={{
+				base: "column",
+				md: "row",
+			}}
+			space={1}
+			alignItems={{
+				base: "center",
+				md: "flex-start",
+			}}
+		>
+			<Avatar
+				size="xl"
+				source={{
+					uri: "https://organicfeeds.com/wp-content/uploads/2021/03/How-To-Raise-A-Baby-Duck-scaled-1.jpg",
+				}}
+			>
+				SS
+			</Avatar>
+		</Stack>
+	);
+};
