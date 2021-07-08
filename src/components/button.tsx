729c0bcb1add9e08a6e9b714b6e2359f2df35c21
Rkeint <keinthraghav@gmail.com>
2021-07-08 17:46:02 +0300

    added validation checks in login page (not working)

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,7 +5,7 @@ import { colors } from "../contants/colors";
 
 type buttonProps = {
 	text: string;
-	press: any;
+	press: () => {};
 };
 
 export const PrimaryButton: React.FC<buttonProps> = ({ text, press }) => {
