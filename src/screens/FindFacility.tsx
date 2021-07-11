01961ea9e5f51407fb12151ac7a1f0438b18481c
ghmeec <39438375+ghmeec@users.noreply.github.com>
2021-07-11 19:17:19 +0300

    added map api key and added layout for animating location

--- src/screens/FindFacility.tsx
+++ src/screens/FindFacility.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { Box, HStack, ScrollView, ZStack } from "native-base";
 
 const FindFacility: React.FC = () => {
     return (
         <View style={styles.container}>
             <MapView
+
                 style={styles.map}
                 initialRegion={{
                     latitude: -3.386319234144578,
@@ -13,22 +15,35 @@ const FindFacility: React.FC = () => {
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
-            />
+            >
+                <Marker
+                    coordinate={{ latitude: -3.386319234144578, longitude: 36.6806454883112 }}
+                    title="Demo hospital"
+                    description="Demo description"
+                />
+            </MapView>
+            <Box h={56}  position="absolute" w="100%" zIndex={2} bottom={0} >
+                <ScrollView horizontal={true} contentContainerStyle={{ alignItems: "center", paddingHorizontal: 10 }} height="100%">
+
+                    {[...new Array(4)].map(el => (
+                        <Box h={48} w={48} borderRadius={6} shadow={2} mx={2} backgroundColor="white">
+                        </Box>
+                    ))}
+
+                </ScrollView>
+            </Box>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	map: {
-		width: Dimensions.get("window").width,
-		height: Dimensions.get("window").height,
-	},
+    container: {
+        flex: 1,
+    },
+    map: {
+        width: "100%",
+        height: "100%",
+    },
 });
 
 export default FindFacility
