93a4f2413e2aa242876d4767dc1196687af25761
iam-kevin <kevin.al.james@gmail.com>
2021-07-14 16:36:13 +0300

    style fix `MainContainer`

--- src/components/containers/MainContainer.tsx
+++ src/components/containers/MainContainer.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, StatusBar, View, Text, ScrollView } from 'native-base';
+import { Box, StatusBar, View, Text, ScrollView, HStack } from 'native-base';
 import _BaseContainer from "./_BaseContainer";
 
 
@@ -9,21 +9,28 @@ export default function MainContainer ({ children, title, leftSection: LeftSecti
 			<StatusBar barStyle="dark-content" backgroundColor={"#fff"} />
 			<Box width={"100%"} flex={1}>
 				{/* Header */}
-				<View flexDirection="row" justifyContent="space-between" paddingX={5} paddingY={8}>
+				<HStack flexDirection="row" justifyContent="space-between" paddingX={5} paddingY={8}>
+					{/* Left section */}
 					{ LeftSection !== undefined ?  (
 						<View>
 							<LeftSection />
 						</View>
 					) : null }
-					<View flexGrow={1} width="100%" flexDirection="row" alignItems="center" justifyContent="center">
-						<Text fontSize="xl">{title}</Text>
-					</View>
+					{/* Title section */}
+					{
+						title !== undefined ? (
+							<Box width="100%" flexDirection="row" alignItems="center" justifyContent="center">
+								<Text fontSize="xl">{title}</Text>
+							</Box>
+						) : null
+					}
+					{/* right section */}
 					{ RightSection !== undefined ?  (
 						<View>
 							<RightSection />
 						</View>
 					) : null }
-				</View>
+				</HStack>
 				{/* Body */}
 				<ScrollView flexGrow={1} showsVerticalScrollIndicator={false}>
 					{ children }
