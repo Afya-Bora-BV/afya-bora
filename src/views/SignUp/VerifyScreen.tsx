d05365d3aa73480f808cc591b9e6c0224b43ee9a
iam-kevin <kevin.al.james@gmail.com>
2021-07-14 12:12:57 +0300

    update `VerifyScreen`

--- src/views/SignUp/VerifyScreen.tsx
+++ src/views/SignUp/VerifyScreen.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
-import { Box, HStack, ScrollView, Stack, Text } from "native-base";
+import { Box, HStack, View, Stack, VStack, Pressable, Text } from "native-base";
 import { PrimaryButton } from "../../components/button";
-import { HeaderwithBack } from "../../components/header";
-import { Spacer } from "../../components/Spacer";
-import { Number } from "../../components/textFields";
 import { colors } from "../../contants/colors";
 import { useState } from "react";
 import { useAuthStore } from "../../internals/auth/context";
+import AltContainer from "../../components/containers/AltContainer";
+
+import { Dimensions } from "react-native";
+
+
 
 export default function VerifyScreen ({ route }: any) {
-	const phoneNumber: string = route.params.phoneNumber
+	const phoneNumber: string = "123123" || route.params.phoneNumber
+	const navigation = useNavigation()
 	const [code, set] = useState<string | undefined>("3") 
+	const { height } = Dimensions.get("screen");
 
 	const confirmCode = useAuthStore(state => state.confirmPhoneCode)
+
 	// do some action when the code is confirmed
 	const onConfirmCode = () => {
 
@@ -30,61 +35,24 @@ export default function VerifyScreen ({ route }: any) {
 	}
 
 	return (
-		<ScrollView>
-			<Stack
-				backgroundColor={colors.primary}
-				borderBottomRadius={40}
-				height={"80%"}
-				position="absolute"
-				top={0}
-				left={0}
-				right={0}
-			></Stack>
-			<Stack paddingBottom={10} paddingTop={10}>
-				<Stack
-					alignItems="center"
-					style={{ paddingBottom: 80, paddingLeft: 25 }}
-				>
-					<HeaderwithBack
-						text="Verify Your Number"
-						onBackPress={() => {
-							navigation.goBack();
-						}}
-						color="white"
-					/>
-				</Stack>
-
-				<Stack alignItems="center">
-					<Box bg="white" shadow={2} rounded={10} width="90%">
-						<Stack paddingBottom={10}>
-							<Spacer size={20} />
-
-							<Stack style={{ paddingHorizontal: 25 }}>
-								<Text style={{ justifyContent: "center" }}>
-									Enter the verification number that was sent
-									to {phoneNumber}
-								</Text>
-							</Stack>
-							<Spacer size={30} />
-							<HStack
-								justifyContent={"space-between"}
-								style={{ paddingHorizontal: 30 }}
-							>
-								{/* <Number number={} /> */}
-							</HStack>
-						</Stack>
-						<Box mb={-6} paddingX={"5%"}>
-							<PrimaryButton
-								text={"Confirm"}
-								press={onConfirmCode}
-							/>
-						</Box>
+		<AltContainer backdropHeight={height / 5.2} navigation={navigation} title="Verify Your Number" headerMode="with-back">
+			<View>
+				<Box bg="white" position="relative" shadow={2} rounded="xl" padding={5} paddingBottom={10} marginX={5} marginBottom={10}>
+					<VStack space={5} marginBottom={15}>	
+						<Text>react-native-confirmation-code-field</Text>
+					</VStack>
+					<Box position="absolute" bottom={-20} left={0} right={0} width="100%" paddingX={10}>
+						<PrimaryButton
+							text={"Confirm"}
+							shadow={5}
+							press={onConfirmCode}
+						/>
 					</Box>
-				</Stack>
-			</Stack>
-			<Stack alignItems="center" marginBottom={5}>
-				<Text color="#2AD3E7">Resend (00:39)</Text>
-			</Stack>
-		</ScrollView>
+				</Box>
+			</View>
+			<View flex={1} alignItems="center" marginBottom={5}>
+		 		<Text color="#2AD3E7">Resend (00:39)</Text>
+		 	</View>
+		</AltContainer>
 	);
 };
