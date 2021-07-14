d2ba977ddfda6c0b9010569afb9b686b81d2ddcb
iam-kevin <kevin.al.james@gmail.com>
2021-07-14 13:43:52 +0300

    ADDED splash screen configuration android; untested

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SplashScreen from 'react-native-splash-screen'
 
 import { extendTheme, NativeBaseProvider } from "native-base";
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
@@ -67,17 +68,18 @@ export const AppTheme = {
 
 function Main() {
 	const user = useAuthStore((state) => state.user);
-	const [isSplashToClose, setSplashToHide] = useState(false);
+	// const [isSplashToClose, setSplashToHide] = useState(false);
 
 	// eecuted when screen is viewed
 	useEffect(() => {
 		// checks from storage, if there is internal state of the user
 		//  if there is or missing, remoce
-		setSplashToHide(true);
+		// setSplashToHide(true);
+		SplashScreen.hide()
 	}, []);
 
 	// Show splash screen if not ready
-	if (!isSplashToClose) return <Splash />;
+	// if (!isSplashToClose) return <Splash />;
 
 	if (user !== null && user !== undefined) {
 		return <AuthenticatedAppView />;
