31d62a9492518a97c8a2cc3dd8311431d99e9820
Rkeint <keinthraghav@gmail.com>
2021-07-16 03:35:56 +0300

    minor fix

--- src/views/Doctor/index.tsx
+++ src/views/Doctor/index.tsx
@@ -1,36 +1,20 @@
-import { createStackNavigator } from "@react-navigation/stack";
-import React from "react";
-import AppointmentInfo from "./AppointmentInfo";
-import DoctorHome from "./Home";
-
-const Stack = createStackNavigator();
-
-export type DoctorStackParamList = {
-	SetAppointmentTime: {
-		consultant: any;
-	};
-	PatientComplaint: {
-		consultant: any;
-		appointment: any;
-	};
-};
-
-export const NavKey = {
-	HomeScreen: "DoctorHome",
-	AppointmentInfoScreen: "AppointmentInfo",
-};
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+import AppointmentInfo from './AppointmentInfo';
+import DoctorHome from './Home';
+import {NavStack, NavKey} from './_navigator';
 
 export default function BookAppointment() {
-	return (
-		<Stack.Navigator
-			headerMode="none"
-			// initialRouteName={NavKey.SetAppointmentTimeScreen}
-		>
-			<Stack.Screen name={NavKey.HomeScreen} component={DoctorHome} />
-			<Stack.Screen
-				name={NavKey.AppointmentInfoScreen}
-				component={AppointmentInfo}
-			/>
-		</Stack.Navigator>
-	);
+  return (
+    <NavStack.Navigator
+      headerMode="none"
+      // initialRouteName={NavKey.SetAppointmentTimeScreen}
+    >
+      <NavStack.Screen name={NavKey.HomeScreen} component={DoctorHome} />
+      <NavStack.Screen
+        name={NavKey.AppointmentInfoScreen}
+        component={AppointmentInfo}
+      />
+    </NavStack.Navigator>
+  );
 }
