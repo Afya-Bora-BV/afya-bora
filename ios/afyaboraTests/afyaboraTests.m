5a8c0bfc280c7485b76ae72f1973b79b77ea7573
iam-kevin <kevin.al.james@gmail.com>
2021-07-16 06:32:36 +0300

    RENAME package to `io.afybora` + add firebase

--- ios/afyaboraTests/afyaboraTests.m
+++ ios/afyaboraTests/afyaboraTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface nativeAppTests : XCTestCase
+@interface afyaboraTests : XCTestCase
 
 @end
 
-@implementation nativeAppTests
+@implementation afyaboraTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
