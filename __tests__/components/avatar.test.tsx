bdbacac1890a684b6caaf80334f05f2892969dcb
Rkeint <37013307+Rkeint@users.noreply.github.com>
2021-07-28 12:35:56 +0300

    added some tests

---
+++ __tests__/components/avatar.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { PicAvatar } from "../../src/components/avatar";
+
+describe("Is the avatar working", () => {
+	test("render test", () => {
+		expect(PicAvatar).toBeDefined;
+	});
+});
