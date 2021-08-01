3c20cca052b687fcac0993bbcc150d9646d06930
Rkeint <37013307+Rkeint@users.noreply.github.com>
2021-08-01 16:47:56 +0300

    top rated specialists. edit appointment WIP

--- src/views/Patient/Profile/VisitHistory/AppointmentSpecifics.tsx
+++ src/views/Patient/Profile/VisitHistory/AppointmentSpecifics.tsx
@@ -1,18 +1,36 @@
-import { ArrowBackIcon, View, VStack } from "native-base";
-import { Text } from "react-native-svg";
+import {
+	ArrowBackIcon,
+	Box,
+	Heading,
+	HStack,
+	Text,
+	View,
+	VStack,
+} from "native-base";
 import React from "react";
 import MainContainer from "../../../../components/containers/MainContainer";
 import { StatusAppointmentAlert } from "../../../../components/core/appointment";
 import { Pressable } from "react-native";
 import { IconContainer } from "../../../../components/misc";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { getAppointmentDetails } from "../../Home/AppointmentInfo";
+import { API_ROOT, getAppointmentDetails } from "../../../../api";
+import axios from "axios";
+import { useQuery } from "react-query";
+import { Spacer } from "../../../../components/Spacer";
 
-export default function AppointmentSpecifics(){
-    const navigation = useNavigation();
+export default function AppointmentSpecifics() {
+	const navigation = useNavigation();
 
 	const route = useRoute();
-	
+
+	const { appointment } = route?.params;
+
+	const { cid, pid } = appointment;
+	const { status, data, error, isLoading } = useQuery(
+		["appointmentDetails", cid, pid],
+		() => getAppointmentDetails({ cid, pid })
+	);
+
 	return (
 		<MainContainer
 			title="Appointment Info"
@@ -41,9 +59,30 @@ export default function AppointmentSpecifics(){
 				{/* <DateTimeCardRender /> */}
 				<View width="100%">
 					<StatusAppointmentAlert
-						time={""}
-						
+						time={data?.date || ""}
+						type={data?.type || "offline"}
+						status={data?.status}
 					/>
+				</View>
+
+				<View bg="white" borderRadius={10} shadow={2} p={5}>
+					<Text fontSize={"2xl"}>Symptoms</Text>
+					<Spacer size={10} />
+					<HStack space={4} flexWrap="wrap">
+						{data?.aboutVisit.symptoms.map((symptom) => (
+							<Box
+								rounded="xl"
+								bg={"#B0B3C7"}
+								flex={1}
+								alignItems="center"
+								paddingY={2}
+							>
+								<Text textAlign="center" color={"white"}>
+									{symptom}
+								</Text>
+							</Box>
+						))}
+					</HStack>
 				</View>
 			</VStack>
 		</MainContainer>
