9a30abfc62213d7b8ce04e91e12d08e52a9db6ac
ghmeec <39438375+ghmeec@users.noreply.github.com>
2021-08-18 12:36:03 +0300

    code cleanup

--- src/views/SelectProfile/_navigator.ts
+++
@@ -1,9 +0,0 @@
-
-import { createStackNavigator } from "@react-navigation/stack"
-
-export const Stack = createStackNavigator()
-
-export const NavKey = {
-    ProfileOption: "Profile.MainOption",
-    ProfileList: "Profile.ListScreen"
-}
