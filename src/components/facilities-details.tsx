de60178157c03a203ece607ea7954c8b11cfd1a9
afyabora911 <89967841+afyabora911@users.noreply.github.com>
2022-12-25 13:16:38 +0400

    update facility info page

--- src/components/facilities-details.tsx
+++ src/components/facilities-details.tsx
@@ -11,6 +11,7 @@ import {
 	Pressable,
 	Avatar,
 	Stack,
+	Center,
 } from "native-base";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import ClipboardPulseIcon from "../assets/icons/ClipboardPulseIcon";
@@ -54,24 +55,25 @@ export const FacilityDetails: React.FC<FacilityDetailsProps> = ({
 		<Box bg="white" shadow={2} rounded={10} testID="ConsultantListItem">
 			<VStack p={4} borderRadius={12} bg={"white"} space={5}>
 				<VStack space={5}>
-					<VStack alignItems="center" justifyContent="center">
+					<Center>
 						<Image
 							source={{
 								uri: photoUrl
 									? photoUrl
 									: "https://firebasestorage.googleapis.com/v0/b/afya-bora-fb.appspot.com/o/c2c820d8-1d2b-4a96-a947-7405156a8f41?alt=media&token=5a364ace-4e71-4b1e-a9f5-38f73b9e24fc",
 							}}
-							alt="Alternate Text" size="xl"
+							alt="Alternate Text" 
 							width={width / 1.2}
-							height={220}
+							height={180}
+							justifyItems="center"
 							borderRadius={4}
 
 						/>
-					</VStack>
+					</Center>
 					<VStack space={5}>
 						<VStack space={0}>
-							<Heading fontSize="lg">{name} </Heading>
-							<Text fontSize="md" bold color="#747F9E">
+							<Heading fontSize="2xl">{name} </Heading>
+							{/* <Text fontSize="md" bold color="#747F9E">
 								{street} ,
 							</Text>
 							<Text fontSize="md" bold color="#747F9E">
@@ -79,7 +81,7 @@ export const FacilityDetails: React.FC<FacilityDetailsProps> = ({
 							</Text>
 							<Text fontSize="md" bold color="#747F9E">
 								{country}.
-							</Text>
+							</Text> */}
 						</VStack>
 
 						<VStack space={5}>
@@ -145,37 +147,50 @@ export const FacilityDetails: React.FC<FacilityDetailsProps> = ({
 							</VStack>
 
 							<VStack>
+								<VStack>
+									<HStack alignItems={"center"} space={2}>
+										<VStack>
+											<MapPinIcon size={6} color={"#000000"} />
+										</VStack>
+
+										<VStack>
+											<Text bold fontSize="md">
+												Location
+											</Text>
+										</VStack>
+									</HStack>
+
+									<VStack pl={7}>
+										<Text  color="#747F9E">
+											{street},
+										</Text>
+										<Text  color="#747F9E">
+											{city},
+										</Text>
+										<Text  color="#747F9E">
+											{country}.
+										</Text>
+									</VStack>
+								</VStack>
 								{geopoint &&
-									<>
-										<HStack alignItems={"center"} space={2}>
-											<VStack>
-												<MapPinIcon size={6} color={"#000000"} />
-											</VStack>
+									<Stack pl={2} mt={2}>
+										<MapView
+											style={{
+												height: 200,
+												width: "100%"
+											}}
+											initialRegion={region}
+										>
+											<Marker
+												coordinate={{ latitude: geopoint.latitude, longitude: geopoint.longitude }}
+												title={name}
+												description={city}
+											/>
 
-											<VStack>
-												<Text bold fontSize="md">
-													Location
-												</Text>
-											</VStack>
-										</HStack>
-										<Stack pl={2} mt={2}>
-											<MapView
-												style={{
-													height: 200,
-													width: "100%"
-												}}
-												initialRegion={region}
-											>
-												<Marker
-													coordinate={{ latitude: geopoint.latitude, longitude: geopoint.longitude }}
-													title={name}
-													description={city}
-												/>
+										</MapView>
 
-											</MapView>
+									</Stack>
 
-										</Stack>
-									</>
 								}
 							</VStack>
 
