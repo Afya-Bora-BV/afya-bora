6f44565c6b517ae4f3f3faa9b7b1c65f4a742152
afyabora911 <89967841+afyabora911@users.noreply.github.com>
2022-12-28 21:58:39 +0400

    Update AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -68,8 +68,6 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
 		const claims = idTokenResult?.claims
 		const isConsultant = (claims?.isConsultant) || (claims?.type === "consultant")
 
-		console.log("IS CONSULTANTS : ",isConsultant)
-	
 		const { collectionName, type } = (isConsultant)
 			? { collectionName: "consultants", type: "consultant" }
 			: { collectionName: "patients", type: "patient" };
